f87fa8f0e18417e7ae5dece0085a346d5e0bf558
artemis34x <hydroxyprop@gmail.com>
2022-09-20 14:56:04 +0200

    loop added

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,10 +3,12 @@ public class Launcher {
         System.out.println("Bienvenue!");
 
         try (var scanner = new java.util.Scanner(System.in)) {
-            String input = scanner.nextLine();
-            if(!"quit".equals(input)){
-                System.out.println("unknown command");
-            }
+            while (true){
+                String input = scanner.nextLine();
+                if(!"quit".equals(input)){
+                    System.out.println("unknown command");
+                }
+            }   
         }
 
     }
